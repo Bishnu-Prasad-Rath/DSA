3deba870c75759877729ba5e435bef80fa3a51d4
Bishnu Prasad Rath <rbishnu604@gmail.com>
2026-07-26 21:10:49 +0530

    Stack question

---
+++ Infosys/6.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool isValid(string s) {
+    stack<char> st;
+    
+    for (char c : s) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        }
+        else {
+            if (st.empty()) return false;
+            
+            char top = st.top();
+            if ((c == ')' && top == '(') ||
+                (c == '}' && top == '{') ||
+                (c == ']' && top == '[')) {
+                st.pop();
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    
+    return st.empty();
+}
+
+int main() {
+    string s;
+    cout << "Enter brackets: ";
+    cin >> s;
+    
+    cout << (isValid(s) ? "Valid!" : "Invalid!") << endl;
+    return 0;
+}
