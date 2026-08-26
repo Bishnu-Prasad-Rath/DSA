ba21e9a6d8f360afbb3bf4374c6b2fd971ddd7ae
Bishnu Prasad Rath <rbishnu604@gmail.com>
2026-08-26 23:22:24 +0530

    binary search using recursion

---
+++ Infosys/16.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+bool binarySearch(int arr[],int size,int key){
+
+
+  
+}
+
+int main(){
+
+int arr[5] = {2,4,6,10,14,16};
+  int size = 6;
+  int key = 6;
+
+bool ans = binarySearch(arr.size,key);
+
+  cout << "The truth whether this key is present in this array or not is : " << ans << endl;
+  
+  return 0;
+}
