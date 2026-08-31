b8ad4314ebe1553025fc80236724d5bb658a11fd
Bishnu Prasad Rath <rbishnu604@gmail.com>
2026-08-31 21:13:47 +0530

    Practice one more time

--- Practice/5.cpp
+++ Practice/5.cpp
@@ -1,2 +1,31 @@
 //Check whether a number is Palindrome
 
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n){
+  if(n < 0) return false;
+  long long reverse, temp = n;
+  while(temp != 0){
+    reverse = reverse * 10 + temp % 10;
+    temp /= 10;
+  }
+  return reverse == n;
+}
+
+int main(){
+
+cout << "Enter the number : " << endl;
+  int n1;
+  cin>>n1;
+
+  bool result = isPrime(n1);
+
+  if(result){
+    cout << "This is a Palindrome" << endl;
+  }else{
+    cout << "This is not a Plindrome" << endl;
+  }
+  
+  return 0;
+}
