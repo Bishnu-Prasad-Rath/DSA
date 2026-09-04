9ace02093ba1d5333504c30a9b1bf5e94431c36c
Bishnu Prasad Rath <rbishnu604@gmail.com>
2026-09-04 16:51:59 +0530

    FindFrequency

--- Practice/24.cpp
+++ Practice/24.cpp
@@ -1,19 +1,23 @@
-//Find the frequency of an particular element
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-int findFrequency(vector<int>& arr,int target){
-  int count = 0;
-  for(int num : arr){
-    if(num == target) count++;
-  }
-  return count;
+int findFrequency(const vector<int>& arr, int target) {
+    int count = 0;
+
+    for (int num : arr) {
+        if (num == target) {
+            ++count;
+        }
+    }
+
+    return count;
 }
 
-int main(){
+int main() {
 
-cout << "Enter the length of the array: ";
+    cout << "Enter the length of the array: ";
 
     int n;
     cin >> n;
@@ -29,12 +33,14 @@ cout << "Enter the length of the array: ";
         arr1.push_back(value);
     }
 
-  cout << "Enter the targeted element : " << endl;
+    cout << "Enter the target element: ";
 
-  int target;
-  cin>>target;
+    int target;
+    cin >> target;
 
-  cout << "The frequency of targeted element is : " << findFrequency(arr1, target) << endl;
-  
-  return 0;
+    cout << "The frequency of the target element is: "
+         << findFrequency(arr1, target)
+         << '\n';
+
+    return 0;
 }
