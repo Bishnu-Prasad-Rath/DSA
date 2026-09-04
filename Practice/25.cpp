b9e69b0295f6d9dff9a81972bde046550837270b
Bishnu Prasad Rath <rbishnu604@gmail.com>
2026-09-04 16:53:22 +0530

    Find the missing number from 1 to N

--- Practice/25.cpp
+++ Practice/25.cpp
@@ -1,18 +1,25 @@
-//Find the missing number from 1 to N
+// Find the missing number from 1 to N
+
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-int findMissing(vector<int>& arr, int n1){
-  long long expectedSum = (long long)n1 * (n1 + 1)/2;
-  long long actualSum = 0;
-  for(int num : arr) actualSum += num;
-  return expectedSum - actualSum;
+long long findMissing(const vector<int>& arr, int n) {
+    long long expectedSum = 1LL * n * (n + 1) / 2;
+
+    long long actualSum = 0;
+
+    for (int num : arr) {
+        actualSum += num;
+    }
+
+    return expectedSum - actualSum;
 }
 
-int main(){
+int main() {
 
-cout << "Enter the length of the array: ";
+    cout << "Enter the length of the array: ";
 
     int n;
     cin >> n;
@@ -21,14 +28,16 @@ cout << "Enter the length of the array: ";
 
     cout << "Enter the elements in this array: ";
 
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n - 1; ++i) {
         int value;
         cin >> value;
 
         arr1.push_back(value);
     }
 
-  cout << "The mising number is : " << findMissing(arr1,n) << endl;
+    cout << "The missing number is: "
+         << findMissing(arr1, n)
+         << '\n';
 
-  return 0;
+    return 0;
 }
