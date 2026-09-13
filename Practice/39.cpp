ab4489d043f3136afa1c4ee826facd82788472c5
Bishnu Prasad Rath <rbishnu604@gmail.com>
2026-09-13 21:59:50 +0530

    Random push

--- Practice/39.cpp
+++ Practice/39.cpp
@@ -16,7 +16,24 @@ vector<int> arrayIntersection(vector<int>& arr1,vector<int>& arr2){
 
 int main(){
 
-
+ cout << "Enter the length of the array: ";
+    int n;
+    cin >> n;
+    
+    // Handle invalid input
+    if(n <= 0){
+        cout << "Invalid array length!" << endl;
+        return 1;
+    }
+    
+    vector<int> arr1;
+    cout << "Enter the elements in this array: ";
+    
+    for(int i = 0; i < n; ++i){
+        int value;
+        cin >> value;
+        arr1.push_back(value);
+    }
 
   return 0;
 }
